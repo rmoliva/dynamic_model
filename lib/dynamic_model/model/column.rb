7827f307d122d39819946246bccad67a6f4b5e80
Roberto M. Oliva <roliva@lciberica.es>
2014-10-27 19:46:48 +0100

    Improved performance: Cached Column definition

--- lib/dynamic_model/model/column.rb
+++ lib/dynamic_model/model/column.rb
@@ -4,11 +4,12 @@ module DynamicModel
       extend ::ActiveSupport::Concern
  
       included do
-        cattr_accessor :column_definitions
+        cattr_accessor :column_definitions do
+          nil
+        end
       end
 
       module ClassMethods
-        
         def dynamic_attribute_scope
           DynamicModel::Attribute.where(:class_type => dynamic_class_type)
         end
@@ -18,19 +19,31 @@ module DynamicModel
         end
         
         # Recorre las definiciones de las columnas que hay de este modelo en la BD
-        def dynamic_column_definitions_each 
-          dynamic_attribute_scope.each do |attribute|
-            yield attribute.to_definition
+        def dynamic_column_definitions_each
+          if !self.column_definitions.nil?
+            self.column_definitions.each do |name, definition|
+              yield definition
+            end
+          else 
+            self.column_definitions = {}
+            dynamic_attribute_scope.each do |attribute|
+              definition = attribute.to_definition
+              
+              self.column_definitions[attribute.name] = definition
+              yield definition
+            end
           end
         end
         
         # Devolver la definicion da un atributo, dado su nombre
         def get_dynamic_column_definition name
-          @@column_definitions ||= {}
-          @@column_definitions[name] = dynamic_attribute_scope.with_name(name).first.try(:to_definition)
+          self.column_definitions ||= {}
+          self.column_definitions[name] = dynamic_attribute_scope.with_name(name).first.try(:to_definition)
         end
         
         def add_dynamic_column params
+          self.column_definitions = nil
+          
           column = dynamic_attribute_scope.with_name(params[:name]).first_or_initialize
           [:type, :length, :required, :default].each do |k|
             column.send("#{k}=", params[k])
@@ -43,8 +56,8 @@ module DynamicModel
             dynamic_attribute_scope.with_name(name).delete_all
             dynamic_value_scope.with_name(name).delete_all
           end
-          @@column_definitions ||= {}
-          @@column_definitions.delete(name)
+          self.column_definitions ||= {}
+          self.column_definitions.delete(name)
         end
         
         def dynamic_column_names
@@ -54,4 +67,4 @@ module DynamicModel
       end
     end
   end
-end
+end 
--- lib/dynamic_model/version_number.rb
+++ lib/dynamic_model/version_number.rb
@@ -1,3 +1,3 @@
 module DynamicModel
-  VERSION = '0.0.14'
+  VERSION = '0.0.15'
 end
