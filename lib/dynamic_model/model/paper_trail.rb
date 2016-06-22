2c2e369d604bae8a69f2e41d9b2804494c2cb92e
Roberto M. Oliva <roliva@lciberica.es>
2016-06-22 13:53:14 +0200

    Temporary removed PaperTrail support

--- lib/dynamic_model/model/paper_trail.rb
+++ lib/dynamic_model/model/paper_trail.rb
@@ -1,21 +1,21 @@
-
-module DynamicModel
-  module Model
-    module PaperTrail
-      extend ::ActiveSupport::Concern
- 
-      included do
-      end
-
-      module ClassMethods
-        def use_dynamic_paper_trail options = {}
-          # Si no hay papertrail o no esta habilitado, no seguir
-          return unless (!defined?(::PaperTrail) or ::PaperTrail.enabled? == false)
-          
-          # Habilitar papertrail para la tabla de atributos
-          Dynamic::Attributes.send(:has_paper_trail, options)
-        end
-      end
-    end
-  end
-end
+#
+# module DynamicModel
+#   module Model
+#     module PaperTrail
+#       extend ::ActiveSupport::Concern
+#
+#       included do
+#       end
+# 
+#       module ClassMethods
+#         def use_dynamic_paper_trail options = {}
+#           # Si no hay papertrail o no esta habilitado, no seguir
+#           return unless (!defined?(::PaperTrail) or ::PaperTrail.enabled? == false)
+#
+#           # Habilitar papertrail para la tabla de atributos
+#           Dynamic::Attributes.send(:has_paper_trail, options)
+#         end
+#       end
+#     end
+#   end
+# end
