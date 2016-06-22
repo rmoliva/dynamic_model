9b000d31fa3df705d531bf12dc085a12c7a7b0ae
Roberto M. Oliva <roliva@lciberica.es>
2016-06-22 13:37:12 +0200

    Temporary removed PaperTrail support

--- lib/dynamic_model/model.rb
+++ lib/dynamic_model/model.rb
@@ -1,14 +1,14 @@
 module DynamicModel
   module Model
     extend ::ActiveSupport::Concern
- 
+
     included do
       include ActiveModel::Naming
       include ActiveModel::Validations
     end
-    
+
     module ClassMethods
-      
+
       # Declare this in your model to use dynamic attributes in this model
       # * options: Not yet
       def has_dynamic_columns(options = {})
@@ -19,14 +19,14 @@ module DynamicModel
         include DynamicModel::Model::Inspect
         include DynamicModel::Model::MethodMissing
         include DynamicModel::Model::Persistence
-        include DynamicModel::Model::PaperTrail
+        # include DynamicModel::Model::PaperTrail
         self
       end
-      
+
       def dynamic_class_type
         self.name
       end
-      
+
     end
   end
 end
